d41de1dc11f747965df401a0c0a365d1b36e8289
NazishQarnain <nazishqarnain@gmail.com>
2026-02-27 22:32:19 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyB9WB0iA4N5WQEM08AmONMSZsxqZ1niC0Y",
+  authDomain: "naz3-game.firebaseapp.com",
+  projectId: "naz3-game",
+  storageBucket: "naz3-game.firebasestorage.app",
+  messagingSenderId: "97109882574",
+  appId: "1:97109882574:web:3733493a188f80a60639db",
+  measurementId: "G-MDMES4P1LS"
+};
